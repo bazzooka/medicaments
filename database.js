923b9770c43bdfd6bbb093337b95a99908df5af6
Joe <bazzooka@gmail.com>
2015-06-27 18:36:25 +0200

    download / parse and insert in db all files

--- database.js
+++ database.js
@@ -132,7 +132,482 @@ var simpleFindTest = function(){
     });
 };
 
+var insertSpecialite = function(datas, callback){
+    console.log("Insert specialite");
+    MongoClient.connect(database_config.url, function(err, db) {
+        var specialite = db.collection('specialite'),
+            datasTab = [];
 
+
+        for(var i = 0, l = datas.length; i < l ; i++){
+            datasTab.push({
+                cip: datas[i][0],
+                denomination: datas[i][1],
+                forme: datas[i][2],
+                voies: datas[i][3],
+                statut_AMM: datas[i][4],
+                type_procedure: datas[i][5],
+                etat: datas[i][6],
+                code_rcp_notice: datas[i][7]
+            });
+        }
+
+        async.each(datasTab, function(item, callback){
+            specialite.insert(item, function(err, result){
+                if(err){
+                    console.log(err);
+                    throw "Erreur in first database specialite";
+                }
+                callback(null, true);
+            });
+        }, function(err){
+            specialite.count(function(err, count) {
+                console.log("%d specialites in database", count);
+                db.close();
+                callback(null, true);
+            });
+        });
+    });
+};
+
+/**
+ * Le nom de ce fichier est : CIS_bdpm.txt
+ Il contient la liste des médicaments commercialisés ou en arrêt de commercialisation depuis
+ moins de trois ans.
+ Les éléments mis à disposition dans ce fichier sont les suivants :
+ - Code CIS (Code Identifiant de Spécialité)
+ - Dénomination du médicament
+ - Forme pharmaceutique
+ - Voies d'administration (avec un séparateur « ; » entre chaque valeur quand il y en a
+ plusieurs)
+ - Statut administratif de l’autorisation de mise sur le marché (AMM)
+ - Type de procédure d'autorisation de mise sur le marché (AMM)
+ - Etat de commercialisation
+ - Date d’AMM (format JJ/MM/AAAA)
+ - StatutBdm : valeurs possibles : « Alerte » (icône rouge) ou « Warning disponibilité »
+ (icône grise)
+ - Numéro de l’autorisation européenne
+ - Titulaire(s) : S’il y a plusieurs titulaires, les valeurs seront séparées par des « ; »
+ - Surveillance renforcée (triangle noir) : valeurs « Oui » ou « Non »
+ * @param datas
+ * @param callback
+ */
+var insertSpecialiteAddon = function(datas, callback){
+    console.log("Insert specialiteAddon");
+    MongoClient.connect(database_config.url, function(err, db) {
+        var specialiteAddon = db.collection('specialiteAddon'),
+            datasTab = [];
+
+
+        for(var i = 0, l = datas.length; i < l ; i++){
+            datasTab.push({
+                cis: datas[i][0],
+                denomination: datas[i][1],
+                forme: datas[i][2],
+                voies: datas[i][3].split(';'),
+                statut_AMM: datas[i][4],
+                type_procedure: datas[i][5],
+                Statut_bdm: datas[i][6],
+                numero_autorisation_eur: datas[i][7],
+                titulaire: datas[i][8].split(';'),
+                surveillance: datas[i][9]
+            });
+        }
+
+        async.each(datasTab, function(item, callback){
+            specialiteAddon.insert(item, function(err, result){
+                if(err){
+                    console.log(err);
+                    throw "Erreur in first database specialiteAddon";
+                }
+                callback(null, true);
+            });
+        }, function(err){
+            specialiteAddon.count(function(err, count) {
+                console.log("%d specialiteAddon in database", count);
+                db.close();
+                callback(null, true);
+            });
+        });
+    });
+};
+
+/**
+ * Le nom de ce fichier est : CIS_CIP_bdpm.txt
+ Il contient la liste des présentations (boîtes de médicaments) disponibles pour les
+ médicaments présents dans le fichier décrit dans le paragraphe 3.1.
+ Les éléments mis à disposition dans ce fichier sont les suivants :
+ - Code CIS
+ - Code CIP7 (Code Identifiant de Présentation à 7 chiffres)
+ - Libellé de la présentation
+ - Statut administratif de la présentation
+ - Etat de commercialisation de la présentation tel que déclaré par le titulaire de l'AMM
+ - Date de la déclaration de commercialisation (format JJ/MM/AAAA)
+ - Code CIP13 (Code Identifiant de Présentation à 13 chiffres)
+ - Agrément aux collectivités ("oui", "non" ou « inconnu »)
+ - Taux de remboursement (avec un séparateur « ; » entre chaque valeur quand il y en a
+ plusieurs)
+ - Prix du médicament en euro
+ - Texte présentant les indications ouvrant droit au remboursement par l’assurance
+ maladie s’il y a plusieurs taux de remboursement pour la même présentation.
+ * @param datas
+ * @param callback
+ */
+var insertPresentation = function(datas, callback){
+    console.log("Insert presentation");
+    MongoClient.connect(database_config.url, function(err, db) {
+        var presentation = db.collection('presentation'),
+            datasTab = [];
+
+
+        for(var i = 0, l = datas.length; i < l ; i++){
+            datasTab.push({
+                cis: datas[i][0],
+                cip7: datas[i][1],
+                libelle: datas[i][2],
+                statut: datas[i][3],
+                date_declaration: datas[i][4],
+                cip13: datas[i][5],
+                agrement_collectivite: datas[i][6],
+                taux_remboursement: datas[i][7].split(';'),
+                prix: datas[i][8],
+                condition_remboursement: datas[i][9]
+            });
+        }
+
+        async.each(datasTab, function(item, callback){
+            presentation.insert(item, function(err, result){
+                if(err){
+                    console.log(err);
+                    throw "Erreur in first database presentation";
+                }
+                callback(null, true);
+            });
+        }, function(err){
+            presentation.count(function(err, count) {
+                console.log("%d presentation in database", count);
+                db.close();
+                callback(null, true);
+            });
+        });
+    });
+};
+
+/**
+ * Le nom de ce fichier est : CIS_COMPO_bdpm.txt
+ Il contient la composition qualitative et quantitative en substances actives et fractions
+ thérapeutiques (telle que figurant dans le Résumé des Caractéristiques du Produit) des
+ médicaments de la BDPM.
+ Les éléments mis à disposition dans ce fichier sont les suivants :
+ - Code CIS
+ - Désignation de l'élément pharmaceutique
+ - Code de la substance
+ - Dénomination de la substance
+ - Dosage de la substance
+ - Référence de ce dosage (exemple : "[pour] un comprimé")
+ - Nature du composant (principe actif : « SA » ou fraction thérapeutique : « ST »)
+ - Numéro permettant de lier, le cas échéant, substances actives et fractions
+ thérapeutiques
+ * @param datas
+ * @param callback
+ */
+
+var insertComposition = function(datas, callback){
+    console.log("Insert composition");
+    MongoClient.connect(database_config.url, function(err, db) {
+        var composition = db.collection('composition'),
+            datasTab = [];
+
+
+        for(var i = 0, l = datas.length; i < l ; i++){
+            datasTab.push({
+                cis: datas[i][0],
+                designation_element: datas[i][1],
+                code_substance: datas[i][2],
+                denomination_substance: datas[i][3],
+                dosage_substance: datas[i][4],
+                reference_dosage: datas[i][5],
+                nature_composant: datas[i][6],
+                numero_liaison: datas[i][7]
+            });
+        }
+
+        async.each(datasTab, function(item, callback){
+            composition.insert(item, function(err, result){
+                if(err){
+                    console.log(err);
+                    throw "Erreur in first database composition";
+                }
+                callback(null, true);
+            });
+        }, function(err){
+            composition.count(function(err, count) {
+                console.log("%d composition in database", count);
+                db.close();
+                callback(null, true);
+            });
+        });
+    });
+};
+
+/**
+ * Le nom de ce fichier est : CIS_HAS_SMR_bdpm.txt
+ Il contient l’ensemble des avis de SMR de la HAS postérieurs à l’année 2002.
+ Les éléments mis à disposition dans ce fichier sont les suivants :
+ - Code CIS
+ - Code de dossier HAS
+ - Motif d’évaluation
+ - Date de l’avis de la Commission de la transparence (format AAAAMMJJ)
+ - Valeur du SMR
+ - Libellé du SMR
+ * @param datas
+ * @param callback
+ */
+var insertAvisSMR = function(datas, callback){
+    console.log("Insert avis SMR");
+    MongoClient.connect(database_config.url, function(err, db) {
+        var avisSMR = db.collection('avisSMR'),
+            datasTab = [];
+
+
+        for(var i = 0, l = datas.length; i < l ; i++){
+            datasTab.push({
+                cis: datas[i][0],
+                code_dossier_HAS: datas[i][1],
+                motif_evaluation: datas[i][2],
+                date_avis: datas[i][3],
+                valeur_SMR: datas[i][4],
+                libelle_SMR: datas[i][5]
+            });
+        }
+
+        async.each(datasTab, function(item, callback){
+            avisSMR.insert(item, function(err, result){
+                if(err){
+                    console.log(err);
+                    throw "Erreur in first database avisSMR";
+                }
+                callback(null, true);
+            });
+        }, function(err){
+            avisSMR.count(function(err, count) {
+                console.log("%d avisSMR in database", count);
+                db.close();
+                callback(null, true);
+            });
+        });
+    });
+};
+
+/**
+ * Le nom de ce fichier est : CIS_HAS_ASMR_bdpm.txt
+ Il contient l’ensemble des avis d’ASMR de la HAS postérieurs à l’année 2002.
+ Les éléments mis à disposition dans ce fichier sont les suivants :
+ - Code CIS
+ - Code de dossier HAS
+ - Motif d’évaluation
+ - Date de l’avis de la Commission de la transparence (format AAAAMMJJ)
+ - Valeur de l’ASMR
+ - Libellé de l’ASMR
+ * @param datas
+ * @param callback
+ */
+var insertAvisASMR = function(datas, callback){
+    console.log("Insert avis ASMR");
+    MongoClient.connect(database_config.url, function(err, db) {
+        var avisASMR = db.collection('avisASMR'),
+            datasTab = [];
+
+
+        for(var i = 0, l = datas.length; i < l ; i++){
+            datasTab.push({
+                cis: datas[i][0],
+                code_dossier_HAS: datas[i][1],
+                motif_evaluation: datas[i][2],
+                date_avis: datas[i][3],
+                valeur_ASMR: datas[i][4],
+                libelle_ASMR: datas[i][5]
+            });
+        }
+
+        async.each(datasTab, function(item, callback){
+            avisASMR.insert(item, function(err, result){
+                if(err){
+                    console.log(err);
+                    throw "Erreur in first database avisASMR";
+                }
+                callback(null, true);
+            });
+        }, function(err){
+            avisASMR.count(function(err, count) {
+                console.log("%d avisASMR in database", count);
+                db.close();
+                callback(null, true);
+            });
+        });
+    });
+};
+
+/**
+ * Le nom de ce fichier est : HAS_LiensPageCT_bdpm.txt
+ Il contient l’ensemble des liens permettant d’accéder aux avis complets de la commission de
+ la transparence pour les SMR et ASMR postérieurs à l’année 2002.
+ Les éléments mis à disposition dans ce fichier sont les suivants :
+ - Code de dossier HAS
+ - Lien vers les pages d’avis de la CT
+ * @param datas
+ * @param callback
+ */
+var insertLienAvis = function(datas, callback){
+    console.log("Insert liens avis");
+    MongoClient.connect(database_config.url, function(err, db) {
+        var liensAvis = db.collection('liensAvis'),
+            datasTab = [];
+
+
+        for(var i = 0, l = datas.length; i < l ; i++){
+            datasTab.push({
+                code_dossier_HAS: datas[i][1],
+                url: datas[i][2]
+            });
+        }
+
+        async.each(datasTab, function(item, callback){
+            liensAvis.insert(item, function(err, result){
+                if(err){
+                    console.log(err);
+                    throw "Erreur in first database liensAvis";
+                }
+                callback(null, true);
+            });
+        }, function(err){
+            liensAvis.count(function(err, count) {
+                console.log("%d liensAvis in database", count);
+                db.close();
+                callback(null, true);
+            });
+        });
+    });
+};
+
+
+/**
+ * Le nom de ce fichier est : CIS_GENER_bdpm.txt
+ Il contient l’ensemble groupes génériques, avec les médicaments en faisant partie.
+ Les éléments mis à disposition dans ce fichier sont les suivants :
+ - Identifiant du groupe générique
+ - Libellé du groupe générique
+ - Code CIS
+ - Type de générique, avec les valeurs suivantes :
+ o 0 : « princeps »
+ o 1 : « générique »
+ o 2 : « génériques par complémentarité posologique »
+ o 4 : « générique substituable »
+ - Numéro permettant de trier les éléments d’un groupe
+ * @param datas
+ * @param callback
+ */
+var insertGeneriques = function(datas, callback){
+    console.log("Insert générique");
+    MongoClient.connect(database_config.url, function(err, db) {
+        var generique = db.collection('generique'),
+            datasTab = [];
+
+
+        for(var i = 0, l = datas.length; i < l ; i++){
+            datasTab.push({
+                identifiant: datas[i][0],
+                libelle: datas[i][1],
+                cis: datas[i][2],
+                type: datas[i][3],
+                ordre: datas[i][4]
+            });
+        }
+
+        async.each(datasTab, function(item, callback){
+            generique.insert(item, function(err, result){
+                if(err){
+                    console.log(err);
+                    throw "Erreur in first database generique";
+                }
+                callback(null, true);
+            });
+        }, function(err){
+            generique.count(function(err, count) {
+                console.log("%d generique in database", count);
+                db.close();
+                callback(null, true);
+            });
+        });
+    });
+};
+/**
+ * Le nom de ce fichier est : CIS_CPD_bdpm.txt
+ Il contient l’ensemble des conditions de prescription et de délivrance associées à un
+ médicament.
+ Les éléments mis à disposition dans ce fichier sont les suivants :
+ - Code CIS
+ - Condition de prescription ou de délivrance
+ * @param datas
+ * @param callback
+ */
+var insertPrescription = function(datas, callback){
+    console.log("Insert prescription");
+    MongoClient.connect(database_config.url, function(err, db) {
+        var prescription = db.collection('prescription'),
+            datasTab = [];
+
+
+        for(var i = 0, l = datas.length; i < l ; i++){
+            datasTab.push({
+                cis: datas[i][0],
+                condition: datas[i][1]
+            });
+        }
+
+        async.each(datasTab, function(item, callback){
+            prescription.insert(item, function(err, result){
+                if(err){
+                    console.log(err);
+                    throw "Erreur in first database prescription";
+                }
+                callback(null, true);
+            });
+        }, function(err){
+            prescription.count(function(err, count) {
+                console.log("%d prescription in database", count);
+                db.close();
+                callback(null, true);
+            });
+        });
+    });
+};
+
+var compareSpecialiteAndAddon = function(){
+    MongoClient.connect(database_config.url, function(err, db) {
+        var specialiteAddon = db.collection('specialiteAddon'),
+            specialite = db.collection('specialite'),
+            datasTab = [];
+
+
+        specialiteAddon.find({}, {cis: 1, _id: 0}).toArray(function(err, docs){
+            var allAddonsCIS = [];
+            for(var i = 0, l = docs.length; i < l; i++){
+                allAddonsCIS.push(docs[i].cis);
+            }
+            specialite.find({'cip': {$nin: allAddonsCIS}}).toArray(function(err, docNotInAddons){
+                console.log(docNotInAddons.length);
+                specialite.count(function(err, count) {
+                    console.log("%d specialites", count);
+                    specialiteAddon.count(function(err, count) {
+                        console.log("%d specialiteAddon", count);
+                        db.close();
+                    });
+                });
+            });
+        });
+    });
+};
 
 module.exports = {
     start : function(ALL_CIP){
@@ -145,7 +620,22 @@ module.exports = {
 
     getAllMedicaments: function(callback){
         getAllMedicaments(callback);
-    }
+    },
+
+    deleteDatabase : deleteDatabase,
+
+    insertSpecialite : insertSpecialite,
+    insertSpecialiteAddon: insertSpecialiteAddon,
+    insertPresentation: insertPresentation,
+    insertComposition: insertComposition,
+    insertAvisSMR: insertAvisSMR,
+    insertAvisASMR: insertAvisASMR,
+    insertLienAvis: insertLienAvis,
+    insertGeneriques: insertGeneriques,
+    insertPrescription: insertPrescription,
+
+
+    compareSpecialiteAndAddon : compareSpecialiteAndAddon
 };
 
 var parseCSVToJSON = function(){
--- parser.js
+++ parser.js
@@ -16,31 +16,34 @@ var
     URL_FICHIER_AVIS_ASMR = "http://base-donnees-publique.medicaments.gouv.fr/telechargement.php?fichier=CIS_HAS_ASMR_bdpm.txt",
     URL_FICHIER_LIENS_AVIS = "http://base-donnees-publique.medicaments.gouv.fr/telechargement.php?fichier=HAS_LiensPageCT_bdpm.txt",
     URL_FICHIER_GENERIQUES = "http://base-donnees-publique.medicaments.gouv.fr/telechargement.php?fichier=CIS_GENER_bdpm.txt",
-    URL_CONDITIONS_PRESCRIPTION = "http://base-donnees-publique.medicaments.gouv.fr/telechargement.php?fichier=CIS_CPD_bdpm.txt";
+    URL_CONDITIONS_PRESCRIPTION = "http://base-donnees-publique.medicaments.gouv.fr/telechargement.php?fichier=CIS_CPD_bdpm.txt",
+
+    filesParams = [
+        {url: URL_FICHIER_SPECIALITE, filename: "specialites.csv",          dbInsertFunc: "insertSpecialite"},
+        {url: URL_FICHIER_SPECIALITE_ADDON, filename: "CIS_bdpm.txt",       dbInsertFunc: "insertSpecialiteAddon"},
+        {url: URL_FICHIER_PRESENTATION, filename: "CIS_CIP_bdpm.txt",       dbInsertFunc: "insertPresentation"},
+        {url: URL_FICHIER_COMPOSITION, filename: "CIS_COMPO_bdpm.txt",      dbInsertFunc: "insertComposition"},
+        {url: URL_FICHIER_AVIS_SMR, filename: "CIS_HAS_SMR_bdpm.txt",       dbInsertFunc: "insertAvisSMR"},
+        {url: URL_FICHIER_AVIS_ASMR, filename: "CIS_HAS_ASMR_bdpm.txt",     dbInsertFunc: "insertAvisASMR"},
+        {url: URL_FICHIER_LIENS_AVIS, filename: "HAS_LiensPageCT_bdpm.txt", dbInsertFunc: "insertLienAvis"},
+        {url: URL_FICHIER_GENERIQUES, filename: "CIS_GENER_bdpm.txt",       dbInsertFunc: "insertGeneriques"},
+        {url: URL_CONDITIONS_PRESCRIPTION, filename: "CIS_CPD_bdpm.txt",    dbInsertFunc: "insertPrescription"}
+    ]
+
+var DATA_STORAGE = [];
 
 
 var generalBaseFileDownloader = function(){
-    var all_url = [
-        URL_FICHIER_SPECIALITE,
-        URL_FICHIER_SPECIALITE_ADDON,
-        URL_FICHIER_PRESENTATION,
-        URL_FICHIER_COMPOSITION,
-        URL_FICHIER_AVIS_SMR,
-        URL_FICHIER_AVIS_ASMR,
-        URL_FICHIER_LIENS_AVIS,
-        URL_FICHIER_GENERIQUES,
-        URL_CONDITIONS_PRESCRIPTION
-    ];
+
     mkdir('./downloaded/', function(err){
         if(err){
             console.log("Error when create -downloaded- folder");
             return true;
         }
-        async.eachSeries(all_url, function(url, callback){
-            var filename = url.match(/=.*/);
-            filename = filename ? filename[0].replace('=', '/') : "specialites.csv";
-            request.get({url: url, encoding: 'binary'}, function (err, response, body) {
-                fs.writeFile('downloaded' + filename, body, 'utf-8', function () {
+        async.eachSeries(filesParams, function(item, callback){
+            var filename = item.filename;
+            request.get({url: item.url, encoding: 'binary'}, function (err, response, body) {
+                fs.writeFile('downloaded/' + filename, body, 'utf-8', function () {
                     console.log(filename + " downloaded");
                     callback();
                 });
@@ -49,5 +52,46 @@ var generalBaseFileDownloader = function(){
     });
 };
 
-generalBaseFileDownloader();
+var parseCSV = function(item, callback){
+    var stream = fs.createReadStream("downloaded/" + item.filename);
+    var csvParser = parse({delimiter: '\t', relax: true});
+    DATA_STORAGE[item.filename] = [];
+
+    csvParser.on('readable', function(){
+        while(record = csvParser.read()){
+            DATA_STORAGE[item.filename].push(record);
+        }
+    });
+
+    csvParser.on('finish', function(){
+        console.log("%s was successfully parsed.", item.filename);
+        console.log(database[item.dbInsertFunc]);
+        database[item.dbInsertFunc] && database[item.dbInsertFunc](DATA_STORAGE[item.filename], callback);
+    });
+
+    stream.pipe(csvParser);
+};
+
+
+var insertInDatabase = function(){
+    database.deleteDatabase(function(){
+        async.each(filesParams, function(item, callback){
+            parseCSV(item, callback);
+        }),
+            function(err){
+                if(err){
+                    throw "Erreur in file parsing";
+                }
+            }
+    });
+};
+
+var createInfos = function(){
+
+}
+
+//generalBaseFileDownloader();
+//insertInDatabase();
+//database.compareSpecialiteAndAddon();
+//createInfos();
 
